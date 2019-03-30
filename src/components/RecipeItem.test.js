7354600c9fbb3f4be2cb0ee0975b64e4da3e07b2
Edinaldo Junior <edinaldojuniorqf@gmail.com>
2019-03-30 12:10:13 -0300

    Tests for RecipeItem.js

--- src/components/RecipeItem.test.js
+++ src/components/RecipeItem.test.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import RecipeItem from './RecipeItem';
 
 describe('RecipeItem', () => {
     test('Should be RecipeItem', () => {
-        const wrapper = mount(<RecipeItem />);
+        const wrapper = mount(<RecipeItem title="" ingredients="" />);
         expect(wrapper.is('RecipeItem')).toBeTruthy();
     })
 
-    // Add more tests here
+    test('Render prop title', () => {
+        const title = 'Ginger Champagne'
+        const wrapper = shallow(<RecipeItem title={title} ingredients="" />)
+        expect(wrapper.text()).toContain(title)
+    })
+
+    test('Render prop ingredients', () => {
+        const ingredients = 'cheddar cheese, eggs, olive oil, onions, potato, salt'
+        const wrapper = shallow(<RecipeItem title="" ingredients={ingredients} />)
+        expect(wrapper.text()).toContain(ingredients)
+    })
+
+    test('Render <img /> with attr src the prop imgSrc', () => {
+        const imgSrc = "image-test.jpg"
+        const wrapper = shallow(<RecipeItem title="" ingredients="" imgSrc={imgSrc} />)
+        const img = wrapper.find('img.card-img-top')
+        expect(img.props().src).toBe(imgSrc)
+    })
+
+    test('Render <img /> with atrr alt the prop title', () => {
+        const title = 'Eggnog Thumbprints'
+        const wrapper = shallow(<RecipeItem title={title} ingredients="" />)
+        const img = wrapper.find('img.card-img-top')
+        expect(img.props().alt).toBe(title)
+    })
 })
