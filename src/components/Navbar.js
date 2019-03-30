c7e0e8e15d7fb4e149882f59f6bf6b7d66183731
Edinaldo Junior <edinaldojuniorqf@gmail.com>
2019-03-30 20:22:08 -0300

    Search in Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,23 +1,42 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import logo from '../logo.svg';
 
-const Navbar = () => (
-  <nav className="navbar fixed-top navbar-expand-sm navbar-dark bg-dark">
-    <div className="navbar-brand col-1">
-      <img src={logo} className="Navbar-logo" alt="logo" />
-    </div>
+class Navbar extends Component {
+  constructor (props) {
+    super(props)
 
-    <div className="form-group justify-content-center row col-10 my-2">
-      <input
-        value=""
-        onChange={(e) => { }}
-        className="form-control col-9 mr-2"
-        type="search"
-        placeholder="Search"
-        aria-label="Search"
-      />
-    </div>
-  </nav>
-)
+    this.handleSearchChange = this.handleSearchChange.bind(this)
+  }
+
+  handleSearchChange (e) {
+    if (this.props.onSearch)
+      this.props.onSearch(e.target.value)
+  }
+
+  render () {
+    return (
+      <nav className="navbar fixed-top navbar-expand-sm navbar-dark bg-dark">
+        <div className="navbar-brand col-1">
+          <img src={logo} className="Navbar-logo" alt="logo" />
+        </div>
+
+        <div className="form-group justify-content-center row col-10 my-2">
+          <input
+            onChange={this.handleSearchChange}
+            className="form-control col-9 mr-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+        </div>
+      </nav>
+    )
+  }
+}
+
+Navbar.propType = {
+  onSearch: PropTypes.function
+}
 
 export default Navbar;
--- src/components/Navbar.test.js
+++ src/components/Navbar.test.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 import Navbar from './Navbar';
 
 describe('Navbar', () => {
   test('Should be App', () => {
     const wrapper = mount(<Navbar />);
     expect(wrapper.is('Navbar')).toBeTruthy();
+  })
+
+  test('Input search call onSearch on onChange', () => {
+    const onSearch = jest.fn()
+
+    const wrapper = mount(<Navbar onSearch={onSearch} />)
+    const wrapperSearch = wrapper.find('input[type="search"]')
+    const value = 'eggs'
+    wrapperSearch.getDOMNode().value = value
+    wrapperSearch.simulate('change')
+    expect(onSearch).toHaveBeenCalledWith(value)
   })
 })
